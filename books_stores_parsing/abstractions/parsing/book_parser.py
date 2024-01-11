4e41df73eae5f6361a7bbb213b465c15ee18e330
Ольга Головкина <GolovkinaOS@bimacad.ru>
2024-01-11 17:41:47 +0300

    Defined the first version of BookParser interface (abstract class)

---
+++ books_stores_parsing/abstractions/parsing/book_parser.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+from pandas import DataFrame
+
+
+class BookParser(ABC):
+
+    @abstractmethod
+    def parse_new_books(self) -> DataFrame:
+        """
+        Parse and returns new books from source.
+        :return: data frame of books with schema:
+        timestamp, store, title, authors, publisher, description, price.
+        If no books are found it will return an empty data frame.
+        """
+        pass
+
+    @abstractmethod
+    def parse_popular_books(self) -> DataFrame:
+        """
+        Parse and returns popular books from source.
+        :return: data frame of books with schema:
+        timestamp, store, title, authors, publisher, description, price.
+        If no books are found it will return an empty data frame.
+        """
+        pass
+
+    @abstractmethod
+    def parse_books_with_discount(self) -> DataFrame:
+        """
+        Parse and returns books with discount.
+        :return: data frame of books with schema:
+        timestamp, store, title, authors, publisher, description, price.
+        If no books are found it will return an empty data frame.
+        """
+        pass
