e92e52c2b61d61ffb9dd61e36c60206855ea2f03
Ольга Головкина <GolovkinaOS@bimacad.ru>
2024-01-16 17:26:10 +0300

    Hadoop test

--- parsing.py
+++ parsing.py
@@ -1,42 +1,56 @@
-import pandas as pd
-from hydra import compose, initialize
+import os
+from pathlib import Path
+from subprocess import PIPE, Popen
 
-from books_stores_parsing.book24.parser import Book24BookParser
-from books_stores_parsing.litres.parser import LitresBookParser
-from books_stores_parsing.multithreading.thread_with_value import ThreadWithValue
-from books_stores_parsing.requesting.rest_requester import RestRequester
+import pandas as pd
+
+# from hydra import compose, initialize
 
 
 def main():
-    initialize(
-        version_base=None, config_path="configs", job_name="books_stores_parsing"
+    # initialize(
+    #     version_base=None, config_path="configs", job_name="books_stores_parsing"
+    # )
+    #
+    # category_ids = compose(config_name="book_categories")
+    # store_ids = compose(config_name="store_ids")
+
+    # rest_req = RestRequester()
+    #
+    # litres_pars = LitresBookParser(rest_req, category_ids, store_ids["litres"])
+    #
+    # book24_pars = Book24BookParser(rest_req, category_ids, store_ids["book24"])
+    #
+    # threads = [
+    #     ThreadWithValue(target=litres_pars.parse_books_all_types),
+    #     ThreadWithValue(target=book24_pars.parse_books_all_types),
+    # ]
+    #
+    # for thread in threads:
+    #     thread.start()
+    #
+    # for thread in threads:
+    #     thread.join()
+    #
+    # res = pd.concat(
+    #     [thread.get_result() for thread in threads if thread.get_result() is not None]
+    # ).reset_index(drop=True)
+
+    res = pd.read_csv("./output/books.txt", sep=";")
+
+    PATH = Path(
+        "/opt/hadoop/airflow/dags/o_golovkina/book_stores_parser/data/books.txt"
     )
+    HADOOP_PATH = Path("/home/o_golovkina/project/parsed_data/books.txt")
 
-    category_ids = compose(config_name="book_categories")
-    store_ids = compose(config_name="store_ids")
+    with open(os.open(PATH.absolute(), os.O_RDWR, mode=777), "w") as file:
+        text_data = res.to_string(header=False, index=False, decimal=";")
+        file.write(text_data)
 
-    selenium_req = RestRequester()
-
-    litres_pars = LitresBookParser(selenium_req, category_ids, store_ids["litres"])
-
-    book24_pars = Book24BookParser(selenium_req, category_ids, store_ids["book24"])
-
-    threads = [
-        ThreadWithValue(target=litres_pars.parse_books_all_types),
-        ThreadWithValue(target=book24_pars.parse_books_all_types),
-    ]
-
-    for thread in threads:
-        thread.start()
-
-    for thread in threads:
-        thread.join()
-
-    res = pd.concat(
-        [thread.get_result() for thread in threads if thread.get_result() is not None]
-    ).reset_index(drop=True)
-
-    res.to_csv("./output/books.txt", sep=";", index=False)  # TODO: положить в hadoop
+    put = Popen(
+        ["hdfs", "dfs", "-put", "-f", PATH, HADOOP_PATH], stdin=PIPE, bufsize=-1
+    )
+    put.communicate()
 
 
 if __name__ == "__main__":
